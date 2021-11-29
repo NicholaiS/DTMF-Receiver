e4e714283e001943dbf09dadea10c5de7e383927
Lubber17 <70474699+Lubber17@users.noreply.github.com>
2021-11-29 19:58:30 +0100

    Update encoder.cpp

--- encoder.cpp
+++ encoder.cpp
@@ -308,6 +308,8 @@ bool encoder::errorcheck(std::string i)
     }
     catch(int x)
     {
+        if(x!=1||x!=2||x!=3||x!=4||x!=5||x!=6)
+            PlaySingle(697,1209);
         switch(x)
         {
         default:
